ffd22584a3846d6b26782cb8644ee382da61cd7f
Ahmet Can Buyukyilmaz <abuyukyi@purdue.edu>
2023-03-16 10:15:55 +0300

    changed naming to PEP 8

--- html_img_embed.py
+++ html_img_embed.py
@@ -1,31 +1,31 @@
 import re
 import base64
 
-def getHTML(filename):
-    with open(f'{filename}', 'r') as FILE:
+def get_html(f):
+    with open(f'{f}', 'r') as FILE:
         html = FILE.read()
     return re.split(r'(<img [^>]*>)', html)
 
 
-def getImg(elem):
-    elem = re.split(r'(src=\"[^\"]*\")', elem)
-    img = elem[1][5:-1]
+def get_img(e):
+    e = re.split(r'(src=\"[^\"]*\")', e)
+    img = e[1][5:-1]
     if img[:4] != 'data':
         with open(img, 'rb') as IMG:
             enc = base64.b64encode(IMG.read())
-        elem[1] = 'src=\"data:image/png;base64,' + enc.decode('utf-8') + '\"'
+        e[1] = 'src=\"data:image/png;base64,' + enc.decode('utf-8') + '\"'
 
-    return ''.join(elem)
+    return ''.join(e)
 
 
 if __name__ == "__main__":
     filename = input("Enter HTML file: ")
 
-    html_text = getHTML(filename)
+    html_text = get_html(filename)
 
     for i, elem in enumerate(html_text):
         if elem[:4] == '<img':
-            html_text[i] = getImg(elem)
+            html_text[i] = get_img(elem)
 
     with open(f'mails/{filename}', 'w') as MAIL:
         MAIL.write(''.join(html_text))
